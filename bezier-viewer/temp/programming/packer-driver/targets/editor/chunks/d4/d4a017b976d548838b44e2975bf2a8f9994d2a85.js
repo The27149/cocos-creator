da2043ebcbd73af596d75742a0221a3724aaeb3e
xiangzhaohua <xiang_zhaohua@163.com>
2024-02-23 16:15:20 +0800

    1

---
+++ bezier-viewer/temp/programming/packer-driver/targets/editor/chunks/d4/d4a017b976d548838b44e2975bf2a8f9994d2a85.js
@@ -0,0 +1,75 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, instantiate, NodePool, _decorator, _class, _crd, ccclass, property, NodeFactory;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      instantiate = _cc.instantiate;
+      NodePool = _cc.NodePool;
+      _decorator = _cc._decorator;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "a6879dPmetKb5ET18ePUefs", "NodeFactory", undefined);
+
+      __checkObsolete__(['instantiate', 'Node', 'NodePool', 'Prefab', '_decorator']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("default", NodeFactory = ccclass(_class = class NodeFactory {
+        constructor(pre) {
+          this.prefab = null;
+          this.nodePool = new NodePool();
+          this.prefab = pre;
+        }
+
+        init(num) {
+          for (let i = 0; i < num; i++) {
+            this.nodePool.put(instantiate(this.prefab));
+          }
+        }
+        /** 获取节点 */
+
+
+        getNode() {
+          let node = null;
+
+          if (this.nodePool.size() > 0) {
+            node = this.nodePool.get();
+          } else {
+            node = instantiate(this.prefab);
+          }
+
+          return node;
+        }
+        /** 放回节点 */
+
+
+        putNode(node) {
+          let test = this.getNode();
+          if (node.name === test.name) this.nodePool.put(node);else console.warn(`节点放回到错误的节点池：`, node, this.nodePool);
+        }
+        /**清空节点池 */
+
+
+        clear() {
+          this.nodePool && this.nodePool.clear();
+        }
+
+      }) || _class);
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=d4a017b976d548838b44e2975bf2a8f9994d2a85.js.map
---
+++ space-fire/assets/script/Main.ts
@@ -0,0 +1,9 @@
+
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Main extends cc.Component {
+
+
+}
